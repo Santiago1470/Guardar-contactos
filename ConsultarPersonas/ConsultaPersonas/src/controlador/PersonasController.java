56b85d3479d78f4697d5c5c0d4fafb2a481c5a16
Santiago1470 <67245823+Santiago1470@users.noreply.github.com>
2023-02-05 13:10:58 -0500

    Add files via upload

--- ConsultarPersonas/ConsultaPersonas/src/controlador/PersonasController.java
+++ ConsultarPersonas/ConsultaPersonas/src/controlador/PersonasController.java
@@ -3,6 +3,7 @@ package controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
@@ -17,107 +18,111 @@ public class PersonasController implements ActionListener {
 	private DAOPersonaImpl daoPersonaImpl;
 	private Persona persona;
 	// private Conexion conexion;
-	
+
 	public PersonasController(Ventana vista, DAOPersonaImpl daoPersonaImpl) {
 		this.vista = vista;
 		this.daoPersonaImpl = daoPersonaImpl;
 		vista.getBtnAgregar().addActionListener(this);
 		vista.getBtnModificar().addActionListener(this);
 		vista.getBtnEliminar().addActionListener(this);
-		
+
 		vista.getInternalFrame().getBtnCRUD().addActionListener(this);
 		vista.getInternalFrame().getBtnCancelarUD().addActionListener(this);
 		vista.getInternalFrame().getBtnBuscar().addActionListener(this);
 	}
-	
+
 	public void iniciar() {
 		vista.setVisible(true);
-		/*String URL = JOptionPane.showInputDialog(null, "Ingrese por favor la URL de la base de datos:");
-		String user = JOptionPane.showInputDialog(null, "Ingrese por favor el usuario de la base de datos:");
-		String password = JOptionPane.showInputDialog(null, "Ingrese por favor la contraseña del usuario de la base de datos:");
-		
-		this.daoPersonaImpl.setURL(URL);
-		this.daoPersonaImpl.setUser(user);
-		this.daoPersonaImpl.setPassword(password);
-		*/
-		
-	    try {
-	    	daoPersonaImpl.conectar();
-	    	mostrarPersonas();
-	    	// JOptionPane.showMessageDialog(null, "Base de datos conectada correctamente.");
-	    	daoPersonaImpl.cerrar();
+		/*
+		 * String URL = JOptionPane.showInputDialog(null,
+		 * "Ingrese por favor la URL de la base de datos:"); String user =
+		 * JOptionPane.showInputDialog(null,
+		 * "Ingrese por favor el usuario de la base de datos:"); String password =
+		 * JOptionPane.showInputDialog(null,
+		 * "Ingrese por favor la contraseña del usuario de la base de datos:");
+		 * 
+		 * this.daoPersonaImpl.setURL(URL); this.daoPersonaImpl.setUser(user);
+		 * this.daoPersonaImpl.setPassword(password);
+		 */
+
+		daoPersonaImpl.conectar();
+		mostrarPersonas();
+		// JOptionPane.showMessageDialog(null, "Base de datos conectada
+		// correctamente.");
+		if (daoPersonaImpl.estadoConexion()) {
+			JOptionPane.showMessageDialog(null, "Base de datos conectada correctamente.");
+		} else {
+			JOptionPane.showMessageDialog(null, "Error al conectar la base de datos.");
+		}
+		try {
+			daoPersonaImpl.cerrar();
 		} catch (Exception ex) {
 			ex.printStackTrace();
-			// JOptionPane.showMessageDialog(null, "Error al conectar la base de datos.\nReinicie el programa, por favor.");
 		}
-	    if(daoPersonaImpl.estadoConexion()) {
-	    	JOptionPane.showMessageDialog(null, "Base de datos conectada correctamente.");
-	    } else {
-	    	JOptionPane.showMessageDialog(null, "Error al conectar la base de datos.");
-	    }
+		
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == vista.getBtnAgregar()) {
+		if (e.getSource() == vista.getBtnAgregar()) {
 			vista.getInternalFrame().setVisible(true);
 			vista.getInternalFrame().mostrarPanel(1);
 			limpiarCampos();
 			mostrarPersonas();
-		} else if(e.getSource() == vista.getBtnModificar()) {
+		} else if (e.getSource() == vista.getBtnModificar()) {
 			vista.getInternalFrame().setVisible(true);
 			vista.getInternalFrame().mostrarPanel(2);
 			limpiarCampos();
 			mostrarPersonas();
-		} else if(e.getSource() == vista.getBtnEliminar()) {
+		} else if (e.getSource() == vista.getBtnEliminar()) {
 			vista.getInternalFrame().setVisible(true);
 			vista.getInternalFrame().mostrarPanel(3);
 			limpiarCampos();
 			mostrarPersonas();
 		}
-		
-		if(e.getSource() == vista.getInternalFrame().getBtnCancelarUD()) {
+
+		if (e.getSource() == vista.getInternalFrame().getBtnCancelarUD()) {
 			limpiarCampos();
 			mostrarPersonas();
 			vista.getInternalFrame().setVisible(false);
 		}
-		
-		if(e.getSource() == vista.getInternalFrame().getBtnCRUD()) {
-			if(vista.getInternalFrame().isFlagAgregar()) {
+
+		if (e.getSource() == vista.getInternalFrame().getBtnCRUD()) {
+			if (vista.getInternalFrame().isFlagAgregar()) {
 				agregarPersona();
 				mostrarPersonas();
-			} else if(vista.getInternalFrame().isFlagModificar()) {
+			} else if (vista.getInternalFrame().isFlagModificar()) {
 				modificarPersona();
 				mostrarPersonas();
-			} else if(vista.getInternalFrame().isFlagEliminar()) {
+			} else if (vista.getInternalFrame().isFlagEliminar()) {
 				eliminarPersonas();
 				mostrarPersonas();
 			}
-		} else if(e.getSource() == vista.getInternalFrame().getBtnBuscar()) {
+		} else if (e.getSource() == vista.getInternalFrame().getBtnBuscar()) {
 			buscarPersona();
 		}
 	}
-	
+
 	private void agregarPersona() {
 		String nombre = vista.getInternalFrame().getTextFieldNombreUD().getText();
 		byte edad = Byte.parseByte(vista.getInternalFrame().getTextFieldEdadUD().getText());
 		long identificacion = Long.parseLong(vista.getInternalFrame().getTextFieldIdUD().getText());
 		String correo = vista.getInternalFrame().getTextFieldCorreoUD().getText();
 		long telefono = Long.parseLong(vista.getInternalFrame().getTextFieldTelUD().getText());
-		
+
 		persona = new Persona(0, nombre, edad, identificacion, correo, telefono);
-		
+
 		try {
 			daoPersonaImpl.agregar(persona);
 		} catch (Exception e) {
-			
+
 			e.printStackTrace();
 		}
-		
+
 		vista.getInternalFrame().setVisible(false);
 		limpiarCampos();
 	}
-	
+
 	private void modificarPersona() {
 		try {
 			String nombre = vista.getInternalFrame().getTextFieldNombreUD().getText();
@@ -125,7 +130,7 @@ public class PersonasController implements ActionListener {
 			long identificacion = Long.parseLong(vista.getInternalFrame().getTextFieldIdUD().getText());
 			String correo = vista.getInternalFrame().getTextFieldCorreoUD().getText();
 			long telefono = Long.parseLong(vista.getInternalFrame().getTextFieldTelUD().getText());
-			
+
 			persona = new Persona(persona.getId(), nombre, edad, identificacion, correo, telefono);
 			daoPersonaImpl.modificar(persona);
 		} catch (Exception ex) {
@@ -134,28 +139,27 @@ public class PersonasController implements ActionListener {
 		vista.getInternalFrame().setVisible(false);
 		limpiarCampos();
 	}
-	
+
 	private void buscarPersona() {
 		boolean existe = false;
 		boolean opcionValida = false;
 		String[] datos = new String[2];
-		
+
 		int item = vista.getInternalFrame().getCmbOpcionBuscar().getSelectedIndex();
-		
-		if(item != 0) {
+
+		if (item != 0) {
 			opcionValida = true;
 		}
-		
-		
-		if(opcionValida) {
+
+		if (opcionValida) {
 			datos[0] = vista.getInternalFrame().getCmbOpcionBuscar().getItemAt(item);
 			datos[1] = vista.getInternalFrame().getTextFieldBuscar().getText();
-			
+
 			try {
 				persona = daoPersonaImpl.buscar(datos);
-				if(persona == null) {
-					JOptionPane.showMessageDialog(null, "La persona consultada con "
-							+ datos[0] + ": " + datos[1] + " no existe.");
+				if (persona == null) {
+					JOptionPane.showMessageDialog(null,
+							"La persona consultada con " + datos[0] + ": " + datos[1] + " no existe.");
 					limpiarCampos();
 					existe = false;
 				} else {
@@ -165,32 +169,32 @@ public class PersonasController implements ActionListener {
 				ex.printStackTrace();
 			}
 		}
-		
-		if(existe) {
+
+		if (existe) {
 			vista.getInternalFrame().getTextFieldNombreUD().setText(persona.getNombre());
 			vista.getInternalFrame().getTextFieldEdadUD().setText(String.valueOf(persona.getEdad()));
 			vista.getInternalFrame().getTextFieldIdUD().setText(String.valueOf(persona.getIdentificacion()));
 			vista.getInternalFrame().getTextFieldCorreoUD().setText(persona.getCorreo());
 			vista.getInternalFrame().getTextFieldTelUD().setText(String.valueOf(persona.getTelefono()));
 		}
-		
-		if(!opcionValida) {
+
+		if (!opcionValida) {
 			JOptionPane.showMessageDialog(null, "Seleccione una opción válida para buscar.");
 			limpiarCampos();
 		}
 	}
-	
+
 	private void eliminarPersonas() {
 		try {
 			daoPersonaImpl.eliminar(persona);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		
+
 		vista.getInternalFrame().setVisible(false);
 		limpiarCampos();
 	}
-	
+
 	private void mostrarPersonas() {
 		try {
 			daoPersonaImpl.mostrar((DefaultTableModel) vista.getTable().getModel());
@@ -199,22 +203,23 @@ public class PersonasController implements ActionListener {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void limpiarCampos() {
 		DefaultTableModel modelo = (DefaultTableModel) vista.getTable().getModel();
-		for(int i = 0; i < vista.getTable().getRowCount(); i++) {
+		for (int i = 0; i < vista.getTable().getRowCount(); i++) {
 			modelo.removeRow(i);
 			i -= 1;
 		}
-		
+
 		vista.getInternalFrame().getTextFieldNombreUD().setText("");
 		vista.getInternalFrame().getTextFieldEdadUD().setText("");
 		vista.getInternalFrame().getTextFieldIdUD().setText("");
 		vista.getInternalFrame().getTextFieldCorreoUD().setText("");
 		vista.getInternalFrame().getTextFieldTelUD().setText("");
-		
-		vista.getInternalFrame().getCmbOpcionBuscar().setSelectedIndex(0);;
+
+		vista.getInternalFrame().getCmbOpcionBuscar().setSelectedIndex(0);
+		;
 		vista.getInternalFrame().getTextFieldBuscar().setText("");
 	}
-	
+
 }
